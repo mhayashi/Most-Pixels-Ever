cc5d996d55ea554287b301c18d707b85a2c06e73
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2012-08-18 00:23:31 +0900

    migrate mpeBouncingBallsExample to oF 007

--- ofxMostPixelsEver/mpeBouncingBallsExample/src/testApp.cpp
+++ ofxMostPixelsEver/mpeBouncingBallsExample/src/testApp.cpp
@@ -2,14 +2,17 @@
 
 //--------------------------------------------------------------
 void testApp::setup() {
-	// initialize app
-	ofSetFrameRate(30);
+    
+	lastFrameTime = ofGetElapsedTimef();
+	
+    // initialize app
+    ofSetVerticalSync(true);
+	ofSetFrameRate(60);
     ofEnableSmoothing();
     ofEnableAlphaBlending();
-	ofSetBackgroundAuto(false);
     
-	client.setup("settings.xml", this, false);
-	
+	client.setup("settings.xml", true);
+    
     // set the random seed
 	ofSeedRandom(1);
     
@@ -19,6 +22,11 @@ void testApp::setup() {
     
 	// start client
     client.start();
+    
+    
+	ofxMPERegisterEvents(this);
+	
+	ofSetBackgroundAuto(false);    
 }
 
 //--------------------------------------------------------------
@@ -27,39 +35,44 @@ void testApp::update() {
 
 //--------------------------------------------------------------
 void testApp::draw() {
-    if (client.isRendering()) {
-        // before we do anything, the client must place itself within the 
-        //  larger display (this is done with translate, so use push/pop if 
-        //  you want to overlay any info on all screens)
-        client.placeScreen();
-        // clear the screen
-        ofBackground(255, 255, 255);
-        
-        // move and draw all the balls
-        for (int i = 0; i < balls.size(); i++) {
-            balls[i]->calc();
-            balls[i]->draw();
-        }
-        
-        // alert the server that you've finished drawing a frame
-        client.done();
-    }
 }
 
 //--------------------------------------------------------------
-// Because we are NOT running in auto mode, no drawing can be
-//  made in this callback function!
-//--------------------------------------------------------------
-void testApp::frameEvent() {
-    // read any incoming messages
-    if (client.messageAvailable()) {
-        vector<string> msg = client.getDataMessage();
-        vector<string> xy = ofSplitString(msg[0], ",");
+void testApp::mpeFrameEvent(ofxMPEEventArgs& event) {
+	//ofClear(0, 0, 0, 0);
+    client.placeScreen();
+	
+    cout << event.message << endl;
+    vector<string> xy = ofSplitString(event.message, ",", true);
+    
+    if (!xy.empty()) {
         float x = ofToInt(xy[0]);
         float y = ofToInt(xy[1]);
         Ball* ball = new Ball(x, y, client.getMWidth(), client.getMHeight());
         balls.push_back(ball);
     }
+    
+    // move and draw all the balls
+    for (int i = 0; i < balls.size(); i++) {
+        balls[i]->calc();
+        balls[i]->draw();
+    }
+		
+	/*
+     float now = ofGetElapsedTimef();
+     cout << "fps would be " << 1./(now - lastFrameTime) << endl;
+     lastFrameTime = now;
+     */
+}
+
+//--------------------------------------------------------------
+void testApp::mpeMessageEvent(ofxMPEEventArgs& event){
+	//received a message from the server
+}
+
+
+void testApp::mpeResetEvent(ofxMPEEventArgs& event){
+	//triggered if the server goes down, another client goes offline, or a reset was manually triggered in the server code
 }
 
 //--------------------------------------------------------------
@@ -94,3 +107,12 @@ void testApp::mouseReleased(int x, int y, int button) {
 void testApp::windowResized(int w, int h) {
 }
 
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+    cout << msg.message << endl;
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
--- ofxMostPixelsEver/mpeBouncingBallsExample/src/testApp.h
+++ ofxMostPixelsEver/mpeBouncingBallsExample/src/testApp.h
@@ -4,28 +4,36 @@
 #include "ofMain.h"
 #include "mpeClientTCP.h"
 #include "Ball.h"
+#include "ofxMostPixelsEver.h"
 
-class testApp : public ofBaseApp, public mpeClientListener {
+class testApp : public ofBaseApp {
 
-	public:
-        void setup();
-        void update();
-        void draw();
+public:
+    void setup();
+    void update();
+    void draw();
         
-        void keyPressed(int key);
-        void keyReleased(int key);
-        void mouseMoved(int x, int y);
-        void mouseDragged(int x, int y, int button);
-        void mousePressed(int x, int y, int button);
-        void mouseReleased(int x, int y, int button);
-        void windowResized(int w, int h);
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
         
-        void frameEvent();
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	
+	mpeClientTCP client;
+	
+	void mpeFrameEvent(ofxMPEEventArgs& event);
+	void mpeMessageEvent(ofxMPEEventArgs& event);
+	void mpeResetEvent(ofxMPEEventArgs& event);
+	float lastFrameTime;
     
-    private:
-        mpeClientTCP  client;
-        vector<Ball*> balls;
-    
+private:
+    vector<Ball*> balls;
+
 };
 
 #endif
